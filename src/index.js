34991613cb1f9939b263743629d741a083323c31
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-07-14 10:42:56 -0300

    fix: add changes - name of the files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const routes = require('./characters.js');
-const connectToDatabase = require('./database.js');
+const routes = require('./characters/character.route');
+const connectToDatabase = require('./database/database');
+const userRoute = require("./users/user.route")
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 connectToDatabase();
 app.use('/characters', routes);
+app.use('/users')
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
