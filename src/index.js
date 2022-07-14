b9c71d106e2ba1809e5e2561dfedad076cb62885
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-07-14 11:29:14 -0300

    chore: add create user controller and service and user routes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./characters/character.route');
 const connectToDatabase = require('./database/database');
-const userRoute = require("./users/user.route")
+const userRoute = require('./users/user.route');
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -11,8 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 connectToDatabase();
+
 app.use('/characters', routes);
-app.use('/users')
+app.use('/users', userRoute);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
